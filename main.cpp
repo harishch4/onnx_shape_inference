94935f59963f3d7af8273d09485fe9fa09de809e
Harish <harish.ch@morphing.in>
2022-04-07 09:41:31 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "onnx/defs/schema.h"
+#include "onnx/defs/shape_inference.h"
+#include "onnx/onnx_pb.h"
+#include "onnx/defs/parser.h"
+#include "onnx/shape_inference/implementation.h"
+
+int main(int, char **) {
+  std::ifstream input("/home/harry/onnx-parser/model.onnx",
+                      std::ios::ate |
+                          std::ios::binary); // open file and move current
+                                             // position in file to the end
+
+  std::streamsize size = input.tellg(); // get current position in file
+  input.seekg(0, std::ios::beg);        // move to start of file
+  std::vector<char> buffer(size);
+  input.read(buffer.data(), size); // read raw data
+
+  onnx::ModelProto model;
+  model.ParseFromArray(buffer.data(), size); // parse protobuf
+  auto graph = model.graph();
+
+  std::cout << model.graph().input().size() << std::endl;
+  ONNX_NAMESPACE::shape_inference::InferShapes(model);
+  std::cout << model.graph().value_info_size() << std::endl;
+}
