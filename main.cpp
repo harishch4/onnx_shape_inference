1b6a9c67cfa2c485d5dfb55add3db34cec589acf
Harish <harish.ch@morphing.in>
2022-04-07 13:05:23 +0530

    print shapes

--- main.cpp
+++ main.cpp
@@ -20,9 +20,22 @@ int main(int, char **) {
 
   onnx::ModelProto model;
   model.ParseFromArray(buffer.data(), size); // parse protobuf
+
+  ONNX_NAMESPACE::shape_inference::InferShapes(model);
   auto graph = model.graph();
 
-  std::cout << model.graph().input().size() << std::endl;
-  ONNX_NAMESPACE::shape_inference::InferShapes(model);
-  std::cout << model.graph().value_info_size() << std::endl;
+  for (size_t i = 0; i < graph.value_info_size(); i++) {
+    const onnx::ValueInfoProto info = graph.value_info(i);
+    std::string name = info.name();
+    auto shape = info.type().tensor_type().shape();
+    if (shape.dim_size() > 0) {
+      int size = shape.dim_size();
+      std::cout << name << " :" << shape.dim(0).dim_param();
+      for (int i = 1; i < size; ++i)
+      {
+        std::cout << shape.dim(i).dim_value() << ", ";
+      }
+      std::cout << std::endl;
+    }
+  }
 }
